bef24de3eee367893691b9e396d17df12abef330
Eric Zhou <ezhou@jadeworld.com>
2024-08-13 12:32:12 +1200

    fix page layout - collapse nav tabs

--- theme.config.jsx
+++ theme.config.jsx
@@ -3,11 +3,21 @@ export default {
     project: {
       link: 'https://github.com/WIEDPUG/wiedpug'
     },
+    sidebar:{
+      defaultMenuCollapseLevel: 1,
+      autoCollapse: true,
+    },
     footer: {
       text: (
         <span>
           WIEDPUG Phase 2 Deliverables Prepared by Jade Software
         </span>
       )
-    }
+    },
+    // feedback: {
+    //   content: null, // Removes the feedback section
+    // },
+    // editLink: {
+    //   component: null, // Removes the "Edit this page" link
+    // },
   }
