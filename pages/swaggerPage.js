d743c0904569bef024cf53834bf98ca40dc28ade
Eric Zhou <ezhou@jadeworld.com>
2024-09-23 21:58:29 +1200

    set schema expand as default

--- pages/swaggerPage.js
+++ pages/swaggerPage.js
@@ -7,17 +7,32 @@ const SwaggerPage = ({url}) => {
     url:url,
     defaultModelsExpandDepth: 1,  
     defaultModelExpandDepth: 2,    
-    docExpansion: "full"           
+    docExpansion:'full'          
   };
 
   useEffect(() => {
-    // This runs after the component is rendered
-    const elements = document.querySelectorAll('aria-expanded');
-    console.log(elements)
-    elements.forEach((button) => {
-      button.click(); // Programmatically click the button
-    });
+    // Set up an interval to check for the buttons
+    const intervalId = setInterval(() => {
+      // Select all buttons with the class 'model-box-control'
+      const buttons = document.querySelectorAll('button.model-box-control');
+
+      if (buttons.length > 0) {
+        console.log('Found buttons:', buttons);
+
+        // Programmatically click each button
+        buttons.forEach((button) => {
+          button.click();
+        });
+
+        // Clear the interval once buttons are found and clicked
+        clearInterval(intervalId);
+      }
+    }, 500); // Check every 500ms
+
+    // Clean up the interval when the component unmounts
+    return () => clearInterval(intervalId);
   }, []);
+
 
   return <SwaggerUI {...swaggerOptions} />;
 };
